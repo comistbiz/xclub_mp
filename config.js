bb093392853e1c1d3193e550388de60f1faf8279
Zug <zugangliu33@gmail.com>
2024-01-07 23:10:28 +0800

    用户填写报名表并提交：更新formSubmit方法

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   version: "4.4.1",
-  api_base_url: 'http://172.16.1.179:8000',
+  api_base_url: 'http://api.vanshin.fun:28080',
   note: '领取优惠券页面优化',
   subDomain: "vanshin", // 根据教程 https://www.it120.cc/help/qr6l4m.html 查看并设置你自己的 subDomain
   baiduMapKey: "TJ3BZ-6LVCF-C25JP-JOA3O-EWZFJ-5FBMI", // 百度地图开放平台去申请自己的key，用来计算配送距离
